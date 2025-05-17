90a2feda0ef2c74400150de1db30ffc9de573227
Masoudi00 <medelwork@example.com>
2025-05-17 12:29:13 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/calories/', // <- Replace with your actual repo name
   plugins: [
     react(),
     tailwindcss()
